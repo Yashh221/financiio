8b080055c4e61066645b97ec26b840e4dbf5803d
Yashvardhan Singh <yash2212003@gmail.com>
2023-05-11 18:06:14 +0530

    mui set

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,7 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true , node:"true"},
   extends: [
+    'react-app',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useMemo} from "react"
+import { BrowserRouter, Routes,Route } from "react-router-dom"
+import { themeSettings } from "./theme"
+import {createTheme} from "@mui/material/styles"
+import { ThemeProvider, CssBaseline ,Box} from "@mui/material"
+import Navbar from "@/scenes/navbar"
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const theme = useMemo(() =>createTheme(themeSettings),[])
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className = "app">
+      <BrowserRouter>
+      <ThemeProvider theme = {theme}>
+        <CssBaseline/>
+        <Box  width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+          <Navbar/>
+          <Routes>
+            <Route path="/" element={<div>dashboard page</div>}/>
+            <Route path="/predictions" element={<div>predictions page</div>}/>
+          </Routes>
+        </Box>
+      </ThemeProvider>
+      </BrowserRouter>
+    </div>
   )
 }
 
