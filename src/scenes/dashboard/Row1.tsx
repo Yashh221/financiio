5e3337f47d9d2b2998ad3b7b4a7345ff96a32707
Yashvardhan Singh <yash2212003@gmail.com>
2023-05-16 17:08:05 +0530

    data added

--- src/scenes/dashboard/Row1.tsx
+++ src/scenes/dashboard/Row1.tsx
@@ -1,13 +1,64 @@
 import DashboardBox from "@/components/DashboardBox";
 import { useGetKpisQuery } from "@/state/api";
+import { useMemo } from "react";
+import { ResponsiveContainer } from "recharts";
+import {
+  AreaChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Area,
+} from "recharts";
+import { useTheme } from "@mui/material";
+
 const Row1 = () => {
-  const {data} = useGetKpisQuery();
-  console.log('data:' ,data)
+  const { palette } = useTheme();
+  const { data } = useGetKpisQuery();
+  // console.log('data:' ,data)
+  const revenueExpenses = useMemo(() => {
+    return (
+      data &&
+      data[0].monthlyData.map(({ month, revenue, expenses }) => {
+        return {
+          name: month.substring(0, 3),
+          revenue: revenue,
+          expenses: expenses,
+        };
+      })
+    );
+  }, [data]);
   return (
     <>
-      <DashboardBox bgcolor="#fff" gridArea="a"></DashboardBox>
-      <DashboardBox bgcolor="#fff" gridArea="b"></DashboardBox>
-      <DashboardBox bgcolor="#fff" gridArea="c"></DashboardBox>
+      <DashboardBox gridArea="a">
+        <ResponsiveContainer width="100%" height="100%">
+          <AreaChart
+            width={500}
+            height={400}
+            data={revenueExpenses}
+            margin={{
+              top: 10,
+              right: 30,
+              left: 0,
+              bottom: 0,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="revenue"
+              stroke={palette.primary.main}
+              fillOpacity={1}
+              fill="url(#colorRevenue"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </DashboardBox>
+      <DashboardBox gridArea="b"></DashboardBox>
+      <DashboardBox gridArea="c"></DashboardBox>
     </>
   );
 };
--- src/state/api.ts
+++ src/state/api.ts
@@ -1,13 +1,13 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
-
+import { GetKpisResponse } from "./types";
 export const api = createApi({
     baseQuery: fetchBaseQuery({baseUrl:import.meta.env.VITE_BASE_URL}),
     reducerPath:"main",
-    tagTypes:["Kpis"],
+    tagTypes:["kpis"],
     endpoints:(build)=>({
-        getKpis:build.query<void,void>({
+        getKpis:build.query<Array<GetKpisResponse>,void>({
             query:()=>"kpi/kpis/",
-            providesTags: ["Kpis"]
+            providesTags: ["kpis"]
         })
     })
 })
