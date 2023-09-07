1336cba6c4ba1954296f916e267bc017e8955b5b
Yashvardhan Singh <yash2212003@gmail.com>
2023-09-07 14:34:16 +0530

    predictions completed

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {createTheme} from "@mui/material/styles"
 import { ThemeProvider, CssBaseline ,Box} from "@mui/material"
 import Navbar from "@/scenes/navbar"
 import Dashboard from "@/scenes/dashboard"
+import Predictions from "./scenes/predictions"
 
 function App() {
   const theme = useMemo(() =>createTheme(themeSettings),[])
@@ -17,7 +18,7 @@ function App() {
           <Navbar/>
           <Routes>
             <Route path="/" element={<Dashboard/>}/>
-            <Route path="/predictions" element={<div>predictions page</div>}/>
+            <Route path="/predictions" element={<Predictions/>}/>
           </Routes>
         </Box>
       </ThemeProvider>
